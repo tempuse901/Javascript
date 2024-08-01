e9089f539cd9eb36806edb08c83e82758e5e71ca
Tempuser <temp.use29901@gmail.com>
2024-08-01 19:28:41 +0530

    files

---
+++ for loop.js
@@ -0,0 +1,18 @@
+// 1st program
+// for(count=10;count>=1;count=count-1){
+//     console.log(count)
+// }
+
+
+// 2nd program
+// for(n=1;n<=10;n=n+1){
+//     if(n%2==0){
+//         console.log(n)
+//     }
+// }
+
+
+// 3rd program
+// for(count=1;count<=20;count=count+1){
+//     console.log(count+"X2="+count*2)
+// }
---
+++ if,else.js
@@ -0,0 +1,126 @@
+//1st program
+// var rain=true
+// if (rain==true)
+// {
+//     console.log("Take an umberalla...")
+// }
+// else
+// {
+//     console.log("Enjoy the sunshine....")
+// }
+
+
+//2nd program
+// var homework="yes"
+// if(homework=="yes")
+// {
+//     console.log("You allow to play games")
+// }
+// else
+// {
+//     console.log("You should to finish the homework")
+// }
+
+//3rd program
+// var cookie="Yes"
+// if(cookie=="Yes")
+// {
+//     console.log("Would you like a cookie?")
+// }
+// else
+// {
+//     console.log("Time to bake more cookies")
+// }
+
+
+//4th program
+// var teeth ="yes"
+// if(teeth=="yes")
+// {
+//     console.log("Your teeth are clean")
+// }
+// else
+// {
+//     console.log("Go brush your teeth now")
+// }
+
+
+//5th program
+// var birthday="Yes"
+// if(birthday=="no"){
+//     console.log("Happy birthday! Enjoy your special day")
+// }
+// else{
+//     console.log("Have a great day")
+// }
+
+
+
+//6st program
+// var color="yellow"
+// if(color=="red"){
+//     console.log("stop")
+// }
+// if(color=="yellow"){
+//     console.log("Get ready")
+// }
+// if(color=="green"){
+//     console.log("Go")
+// }
+
+//7nd program
+// var season="fall"
+// if(season=="spring"){
+//     console.log("Enjoy the blooming flowers")
+// }
+// if(season=="summer"){
+//     console.log("have fun in the sun")
+// }
+// if(season=="autumn" || season=="fall"){
+//     console.log("Admire the colorful leaves")
+// }
+// if(season=="winter"){
+//     console.log("Bundle up and stay warm")
+// }
+
+
+//8rd program
+// var score=89
+// if(score<50){
+//     console.log("You need to improve")
+// }
+// if(50>score>70){
+//     console.log("Good job!")
+// }
+// if(score>70){
+//     console.log("Excellent performance!")
+// }
+
+
+// //9th program
+// var number=62973
+// if(number%2==0){
+//     console.log("The number is even")
+// }
+// else{
+//     console.log("The number is odd")
+// }
+
+
+// // 10th program
+// var character="a"
+// if(character=="a" || character=="e" || character=="i" || character=="o" ||character=="u"){
+//     console.log("it's a vowel")
+// }
+// else{
+//     console.log("it's a constant")
+// }
+
+
+
+// var a=10
+// {
+//     let b=100
+// }
+// console.log(a+b)
+
